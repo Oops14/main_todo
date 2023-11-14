27f3761778146b188cd7cf5c50816cee61a7de3d
Oops14 <ups14q@gmail.com>
2023-11-14 21:40:15 +0200

    checked functionality

---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+type TodoFormType = {
+    addTodo: (addTodo: any) => void
+}
+
+export const TodoForm = (props: TodoFormType) => {
+    const [item, setItem] = useState('');
+    // React.useEffect(() => console.log("data", item), [item]);
+
+
+    return (
+        <div className="todo-form">
+            <input value={item} onChange={(e) => {
+                e.preventDefault();
+
+                setItem(e.target.value);
+            }} />
+            <button onClick={() => {
+                props.addTodo(item);
+                setItem('');
+            }}>+</button>
+        </div>
+    );
+};
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import { TodoForm } from "./TodoForm";
 
+type TodosType = {
+    id: string;
+    todoItemName: string;
+    isDone?: boolean;
+};
 
 export const TodoList = () => {
+    const [todos, setTodos] = useState<Array<TodosType>>([]);
+    React.useEffect(() => console.log("data", todos), [todos]);
+
+    let addTodo = (addTodo: string) => {
+        setTodos([
+            ...todos,
+            {
+                id: uuidv4(),
+                todoItemName: addTodo,
+                isDone: false,
+            },
+        ]);
+    };
+
     return (
         <div>
             <h3>What to learn</h3>
-            <div>
-                <input />
-                <button>+</button>
-            </div>
-            <ul>
-                <li>
-                    <input type="checkbox" checked={true} />{" "}
-                    <span>HTML&CSS</span>
-                </li>
-                <li>
-                    <input type="checkbox" checked={true} /> <span>JS</span>
-                </li>
-                <li>
-                    <input type="checkbox" checked={false} /> <span>React</span>
-                </li>
+            <TodoForm addTodo={addTodo} />
+            <ul className="todo-list">
+                {todos.map((item) => {
+                    return (
+                        <li key={item.id}>
+                            <input
+                                type="checkbox"
+                                onClick={() => {
+                                    item.isDone
+                                        ? (item.isDone = false)
+                                        : (item.isDone = true);
+                                }}
+                            />
+                            <span>{item.todoItemName}</span>
+                        </li>
+                    );
+                })}
             </ul>
             <div>
                 <button>All</button>
